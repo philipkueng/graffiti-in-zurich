6a2a60d6aae1efed6b76870c619e88b332677b30
Philip Küng <kueng.philip@gmail.com>
2021-03-06 18:23:00 +0100

    add function to generate report by data

---
+++ graffiti-data/interpret_data.js
@@ -0,0 +1,115 @@
+const fsWrite = require('fs');
+const fs = require('fs').promises;
+
+(async () => {
+  try {
+    let report = '';
+    let rawData = await getRawData('./data/processedData.json');
+    let frequencyByYear = getFrequencyByYear(rawData);
+    let frequencyByDistrict = getFrequencyByDistrict(rawData);
+    report += getFrequencyByYearReport(frequencyByYear);
+    report += getFrequencyByDistrictReport(frequencyByDistrict);
+    writeData(report);
+  } catch (e) {
+    console.log(e)
+  }
+})();
+
+function getFrequencyByYear(data) {
+  let frequencyByYear = [];
+  data.forEach(feature => {
+    if (feature) {
+      frequencyByYear.push({ year: feature.year, totalReports: feature.collection.length })
+    }
+  })
+
+  // sort by most to least
+  return frequencyByYear.sort(function (a, b) {
+    return b.totalReports - a.totalReports;
+  });
+}
+
+function getFrequencyByYearReport(data) {
+  let report = '## Most frequent year\n';
+  data.forEach((feature, index) => {
+    if (index + 1 === data.length) {
+      report += `The year with the least graffitis is ${feature.year} with ${feature.totalReports}\n\n`
+    } else if (index === 0) {
+      report += `\nThe year with the most graffitis is ${feature.year} with ${feature.totalReports}\n`
+    } else {
+      report += `The year with the ${index + 1}. most graffitis is ${feature.year} with ${feature.totalReports}\n`
+    }
+  })
+  return report;
+}
+
+function getFrequencyByDistrict(data) {
+  let frequencyByDistrict = [];
+  data.forEach(feature => {
+    let year = feature.year;
+    let existingYear = frequencyByDistrict.find(item => item.year === year);
+    if (!existingYear) {
+      frequencyByDistrict.push({
+        year, districts: [
+          { "name": "Kreis 1", totalReports: 0 },
+          { "name": "Kreis 2", totalReports: 0 },
+          { "name": "Kreis 3", totalReports: 0 },
+          { "name": "Kreis 4", totalReports: 0 },
+          { "name": "Kreis 5", totalReports: 0 },
+          { "name": "Kreis 6", totalReports: 0 },
+          { "name": "Kreis 7", totalReports: 0 },
+          { "name": "Kreis 8", totalReports: 0 },
+          { "name": "Kreis 9", totalReports: 0 },
+          { "name": "Kreis 10", totalReports: 0 },
+          { "name": "Kreis 11", totalReports: 0 },
+          { "name": "Kreis 12", totalReports: 0 },
+        ]
+      })
+    }
+    // get year within data
+    existingYear = frequencyByDistrict.find(item => item.year === year);
+    // iterate through collection and add district to existing year.totalReports
+    feature.collection.forEach(info => {
+      let district = existingYear.districts.find(dis => dis.name === info.district);
+      if (!district) {
+        console.log(`There is something weird going on with ${info}`);
+      }
+      district.totalReports += 1;
+    })
+  });
+
+  // sort by newest to oldest
+  return frequencyByDistrict.sort(function (a, b) {
+    return b.year - a.year;
+  });
+}
+
+function getFrequencyByDistrictReport(data) {
+  let report = '## District frequency (newest to oldest) \n\n';
+  data.forEach(feature => {
+    report += `### District frequency in ${feature.year} (most to least)\n\n`;
+    let districts = feature.districts.sort(function (a, b) {
+      return b.totalReports - a.totalReports;
+    });
+    districts.forEach((district, index) => {
+      report += `${index + 1}. ${district.name} with a total report of ${district.totalReports} cases\n`;
+      if (index + 1 === districts.length) {
+        report += '\n';
+      }
+    })
+  })
+  return report;
+}
+
+async function getRawData(path) {
+  let data = await fs.readFile(path, 'utf8');
+  return JSON.parse(data);
+}
+
+function writeData(data) {
+  try {
+    fsWrite.writeFileSync('./data/report.md', data);
+  } catch (err) {
+    console.log('err writing in file: ' + err.message)
+  }
+}
